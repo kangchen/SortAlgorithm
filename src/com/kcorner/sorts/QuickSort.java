ff47fab8915f7841185277aeead737b4db946a94
Kang Chen <kangchen@kcorner.com>
2012-12-31 19:45:26 -0800

    initial commit

---
+++ src/com/kcorner/sorts/QuickSort.java
@@ -0,0 +1,123 @@
+package com.kcorner.sorts;
+
+public class QuickSort implements Runnable{
+	
+	private int[] intArray;
+	private long time = 0;
+	private int pivotPos=0;
+	
+	public QuickSort(int[] arr) {
+		intArray = arr;
+	}
+	
+	public void run() {		
+		sort();	
+		System.out.println(this.toString());
+	}
+	
+    /*
+     * Quick Sort Algorithm
+     */
+    public void sort() {
+    	
+        long starttime = System.currentTimeMillis();
+        if (!isSorted()){
+            doQuickSort(0, intArray.length-1);
+        }
+        long endtime = System.currentTimeMillis();
+        time = endtime - starttime;
+    }
+
+    private void doQuickSort(int leftIdx, int rightIdx) {
+
+        if (rightIdx <= leftIdx) {
+            return;
+        }
+        else {
+            pivotPos = quickPartition(leftIdx, rightIdx);
+            doQuickSort(leftIdx, pivotPos-1);
+            doQuickSort(pivotPos+1, rightIdx);
+        }
+
+    }
+
+    private int quickPartition(int leftIdx, int rightIdx)
+    {
+
+        int pivot = intArray[leftIdx];
+        int pivotIdx = leftIdx;
+        
+        while (rightIdx > leftIdx) {
+
+            while (intArray[rightIdx] > pivot) {
+                if (rightIdx == leftIdx) break;
+                else rightIdx--;
+            }
+
+            while (intArray[leftIdx] <= pivot) {
+                if (leftIdx == rightIdx) break;
+                else leftIdx++;
+            }
+
+            if(leftIdx < rightIdx){
+                swap(leftIdx, rightIdx);
+                rightIdx--;
+            }
+
+            if(leftIdx == rightIdx) {
+                if(intArray[leftIdx] < pivot) {
+                    swap(pivotIdx, leftIdx);
+                }
+                pivotIdx = leftIdx;
+                break;
+            }
+        }
+
+        return pivotIdx;
+    }
+ 
+
+    public boolean isSorted(){
+        long starttime = System.currentTimeMillis();
+        long endtime = 0;
+
+        if(intArray[0] < intArray[intArray.length-1]) {
+            for(int i=0; i<intArray.length-1; i++){
+                if(intArray[i] > intArray[i+1]) {
+                    endtime = System.currentTimeMillis();
+                    time = endtime - starttime;
+                    return false;
+                }
+            }
+        }
+        else {
+            for(int i=0; i<intArray.length-1; i++){
+                if(intArray[i] < intArray[i+1]) {
+                    endtime = System.currentTimeMillis();
+                    time = endtime - starttime;
+                    return false;
+                }
+            }
+        }
+
+        endtime = System.currentTimeMillis();
+        time = endtime - starttime;
+        return true;
+    }
+    
+
+    private void swap(int x, int y) {
+        int tmp = intArray[x];
+        intArray[x] = intArray[y];
+        intArray[y] = tmp;
+    }
+    
+    public String toString() {
+    	return "QuickSort is completed: " + getTime() + " milliseconds";
+    }
+
+    private long getTime() {
+        return time;
+    }
+	
+}
---
+++ src/com/kcorner/sorts/SortMain.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kcorner.sorts;
+import acm.program.*;
+
+/**
+ *
+ */
+public class SortMain extends ConsoleProgram{
+
+	private static final long serialVersionUID = 1L;
+	private static final int XSCREENSIZE = 800;
+    private static final int YSCREENSIZE = 600;
+    public static int SIZE = 19;
+    public static int MAXINT = 80;
+    private int[] a = new int[SIZE];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new SortMain().start(args);
+    }
+
+    @Override
+    public void init() {
+
+    	setRandomNumber();
+        setFont("verdana-bold-14");
+        setSize(XSCREENSIZE, YSCREENSIZE);
+        setTitle("Sort Algorithms");
+        
+    }
+    
+    private void setRandomNumber() {
+        for(int i=0;i<SIZE;i++){
+            a[i] = (int)(Math.random()*MAXINT) + 10;
+        }
+    }
+
+    @Override
+    public void run() {
+    	test();
+    	println("Done");
+    }
+    
+    private void test() {
+
+      println(a);	
+//    Sort mySort = new Sort(a); 
+//    mySort.quickSort();
+//    mySort.mergeSort();
+//    mySort.selectionSort();
+//    mySort.bubbleSort();      
+   	
+      //Using thread
+      QuickSort myQuickSort = new QuickSort(a);
+      Thread t = new Thread(myQuickSort);
+      try{
+    	  t.start();
+    	  t.join();
+      }catch(Exception e){
+    	  System.out.println(e.getMessage());
+      }
+      //////////////////////////////
+
+      println(a);
+    }
+    
+    private void println(int[] ab) {
+        for (int a:ab) print(a + "  ");
+        println();
+    }
+    
+}
