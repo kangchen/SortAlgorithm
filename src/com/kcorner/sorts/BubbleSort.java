2e0e0c8bbe83eed635bcd165b225c4c8671f632b
Kang Chen <kang_chen@msn.com>
2013-01-03 04:16:46 -0800

    BubbleSort File Added

---
+++ src/com/kcorner/sorts/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.kcorner.sorts;
+
+public class BubbleSort implements Runnable{
+	
+	
+	private int[] item;
+
+	public void run() {
+		for (int i=0; i<item.length-1; i++){
+			for (int j=0; j<item.length-1; j++){
+				if (item[j] > item[j+1]) {
+					swap(j,j+1);
+				}
+			}
+		}
+	}
+    
+    private void swap(int x, int y) {
+        int tmp = item[x];
+        item[x] = item[y];
+        item[y] = tmp;
+    }
+}
