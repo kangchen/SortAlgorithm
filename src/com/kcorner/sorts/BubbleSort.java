8b3ece6e270bd11f89c1b7e06f9bc9f5bbcf01fc
Kang Chen <kang_chen@msn.com>
2013-01-03 18:38:52 -0800

    code changes

--- src/com/kcorner/sorts/BubbleSort.java
+++ src/com/kcorner/sorts/BubbleSort.java
@@ -4,20 +4,33 @@ public class BubbleSort implements Runnable{
 	
 	
 	private int[] item;
+	
+	public BubbleSort() {
+		
+	}
 
 	public void run() {
+		sortIt();
+	}
+    
+	public void sortIt() {
+		
 		for (int i=0; i<item.length-1; i++){
 			for (int j=0; j<item.length-1; j++){
 				if (item[j] > item[j+1]) {
 					swap(j,j+1);
 				}
 			}
-		}
+		}		
 	}
-    
+	
     private void swap(int x, int y) {
         int tmp = item[x];
         item[x] = item[y];
         item[y] = tmp;
+    }
+    
+    public String toString() {
+    	return null;
     }
 }
